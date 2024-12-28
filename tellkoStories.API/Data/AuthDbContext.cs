06193c8474b4eaaa8c0055ba1c7a5942a0aa5267
Izet Delibasic <izetdelibasic55@gmail.com>
2024-12-28 17:39:56 +0100

    Creating AuthDbContext, adding seed roles and seed default user

---
+++ tellkoStories.API/Data/AuthDbContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace tellkoStories.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "7512a89d-00d9-4b98-9368-de3a6ea1348e";
+            var writerRoleId = "ec052d97-2dc8-4547-896f-e1a1cab45f24";
+
+            // Creating Reader and Writer Role
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper(),
+                    ConcurrencyStamp = readerRoleId,
+                },
+                new IdentityRole()
+                {
+                    Id = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper(),
+                    ConcurrencyStamp = writerRoleId,
+                },
+            };
+
+            // Seeding the roles
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // Creating an Admin User
+            var adminUserId = "9cbf7c8c-d828-42e8-8431-4d6174adafb6";
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                NormalizedEmail = "admin@gmail.com".ToUpper(),
+                NormalizedUserName = "admin@gmail.com".ToUpper()
+            };
+
+            admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin@123");
+
+            builder.Entity<IdentityUser>().HasData(admin);
+
+            // Giving Roles to Admin
+
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = readerRoleId,
+                },
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = writerRoleId,
+                }
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+        }
+    }
+}
